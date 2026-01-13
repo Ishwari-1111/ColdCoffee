29d482f3872ff9d73d11ed023ff98c3d05110e57
Ishwari Shekade <146479060+Ishwari-1111@users.noreply.github.com>
2026-01-13 15:55:24 +0530

    Create hello.java with main method and print statements

---
+++ hello.java
@@ -0,0 +1,8 @@
+class main
+  {
+    public static void main(String[] args)
+    {
+      system.out.println("hello, this is a new file of a new branch b1");
+       system.out.println("Byee");  
+    }
+  }
